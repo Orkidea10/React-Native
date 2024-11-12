33526c43b2e19ed5d84b9422070531982cdbf656
Orkidea10 <orkideaselmanaj10@gmail.com>
2024-11-12 20:45:46 +0100

    Module3

---
+++ Module3/first-react-project/screens/MainScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Text, StyleSheet, View} from "react-native";
+const MainScreen = ()=>{
+    return <View>
+        <Text>This is Main screen</Text>
+    </View>
+}
+export default MainScreen;

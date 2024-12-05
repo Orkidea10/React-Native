71c59329aa85e45c3577da6830074441d322a29c
Orkidea10 <orkideaselmanaj10@gmail.com>
2024-12-05 21:16:37 +0100

    Challenge

--- Module9/challenge/App.js
+++ Module9/challenge/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChallengeScreen from './screens/challengeScreen';
+import ChallengeScreen from './screens/ChallengeScreen1';
 
 export default function App() {
   return(
